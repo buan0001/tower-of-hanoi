fe19f1faa686b32a2db75796672baaa871b5eb64
Lise Freja Jensen Petculescu <124042840+LisePetculescu@users.noreply.github.com>
2024-10-26 20:34:50 +0200

    Everything done, except for SOLVER and RESETTER

--- script.js
+++ script.js
@@ -1,70 +1,105 @@
+import Stack from "./Stack.js";
+
 window.addEventListener("load", start);
 
-const RING_AMOUNT = 5;
+const RING_AMOUNT = 3;
+let moves_used = 0;
+const pins = { pin1: new Stack(), pin2: new Stack(), pin3: new Stack() };
+// const pin1 = new Stack();
+// const pin2 = new Stack();
+// const pin3 = new Stack();
 
 function start() {
-  const nodes = document.querySelectorAll(".ring");
   const pins = document.querySelectorAll(".pin");
 
   pins.forEach((pin) => {
-    pin.addEventListener("click", pinClicked);
+    pin.addEventListener("click", () => pinClicked(pin.id));
   });
-  console.log("nodes length", nodes.length);
+  document.querySelector("#solve-btn").addEventListener("click", solveTowers);
 
-  nodes.forEach((node, index) => {
-    const widthVal = (index + 1) * 50 + "px";
-    // const widthVal = (index+1)*20 + "px"
-    const bottomVal = (nodes.length - index - 1) * 30 + "px";
-    console.log("Heigh val:", widthVal);
-    node.style.setProperty("--RING_WIDTH", widthVal);
-    node.style.setProperty("--RING_BOTTOM", bottomVal);
-  });
-
-  createRings()
+  createRings();
+  updateDisplay();
 }
 
 function createRings() {
-  const firstPin = document.querySelector("#pin1");
-
-  for (let index = 0; index < 5; index++) {
-    const element = document.createElement("div");
-
-    element.classList.add("ring")
-    element.dataset.value = index
-    element.style.setProperty("--RING_WIDTH", (index + 1) * 50 + "px");
-    element.style.setProperty("--RING_BOTTOM", (RING_AMOUNT - index - 1) * 30 + "px");
-
-    
-    firstPin.insertAdjacentElement("beforeend", element)
-    console.log("Element:",element);
+  for (let index = 0; index < RING_AMOUNT; index++) {
+    pins.pin1.push(RING_AMOUNT - index);
+    // console.log(pins.pin1.size());
   }
+}
+
+function updateDisplay() {
+  document.querySelector("#moves-display").innerHTML = moves_used;
+  for (const pin in pins) {
+    const firstPin = document.querySelector(`#${pin}`);
+
+    firstPin.innerHTML = "";
+    // console.log(pin);
+    let nextNode = pins[pin].peek(); // pins[pin]
+    let hiddenRings = pins[pin].size() - 1;
+    while (nextNode) {
+      const element = document.createElement("div");
+      element.classList.add("ring");
+      element.style.setProperty("--RING_WIDTH", nextNode.data * 28 + "px");
+      // element.style.setProperty("--RING_BOTTOM", (hiddenRings - nextNode.data) * 30 + "px");
+      element.style.setProperty("--RING_BOTTOM", hiddenRings-- * 30 + "px");
+      firstPin.insertAdjacentElement("beforeend", element);
+
+      nextNode = nextNode.next;
+    }
+
+    // const amountOfRings = pins[pin].size();
+    // for (let index = 0; index < amountOfRings; index++) {
+    //   const element = document.createElement("div");
+
+    //   element.classList.add("ring");
+    //   element.style.setProperty("--RING_WIDTH", (index + 1) * 27 + "px");
+    //   element.style.setProperty("--RING_BOTTOM", (amountOfRings - index - 1) * 30 + "px");
+
+    //   firstPin.insertAdjacentElement("beforeend", element);
+    //   console.log("Element:", element);
+    // }
+  }
+}
+
+function toggleSelectedDisplay(pinName) {
+  const firstPin = document.querySelector(`#${pinName}`);
+  const firstRing = firstPin.firstElementChild;
+  firstRing.classList.toggle("selected");
 }
 
 let selected = null;
-function pinClicked(e) {
-  console.log("pin clicked, event:", e);
-  const pin = e.target;
-  if (selected) {
-    pin.appendChild(selected);
-    selected.classList.remove("selected");
-    updateRingPositions(pin);
-    selected = null;
-  } else {
-    const topRing = pin.firstElementChild;
-    const ringSize = topRing.dataset.value;
-    console.log("Ring size:", ringSize);
+function pinClicked(pinId) {
+  console.log("pin clicked, event:", pinId);
 
-    if (topRing) {
-      topRing.classList.add("selected");
-      selected = topRing;
+  if (selected) {
+    const newStack = pins[pinId];
+    if (newStack == selected) {
+      toggleSelectedDisplay(pinId);
+      selected = null;
+      return;
     }
+    const ringToMove = selected.peek();
+    if (ringToMove?.data < newStack.peek()?.data || newStack.peek() == null) {
+      const movingRing = selected.pop();
+      newStack.push(movingRing.data);
+      moves_used++;
+      updateDisplay();
+      selected = null;
+      if (newStack == pins["pin3"] && newStack.size() == RING_AMOUNT) {
+        console.log("you win!");
+      }
+    }
+  } else {
+    selected = pins[pinId];
+    toggleSelectedDisplay(pinId);
   }
 }
 
-function updateRingPositions(pin) {
-  const rings = pin.querySelectorAll(".ring");
-  rings.forEach((ring, index) => {
-    const bottomVal = index * 30 + "px";
-    ring.style.setProperty("--RING_BOTTOM", bottomVal);
-  });
+
+// TODO: Make solver
+
+function solveTowers(){
+  console.log("solving towers!");
+  
 }
